d60208ca2176aef5f69cfe80a044dcf1e1e7e44d
Solomon Victorino <solomonvictorino@gmail.com>
2019-04-11 17:46:43 -0700

    Output a UDC fallback ratio
    which is equal to the number of failed AIML matches divided by the total number of responses

--- pandorabots-udc-count.py
+++ pandorabots-udc-count.py
@@ -15,12 +15,16 @@ if log_filename is None:
 with open(log_filename) as fd:
     rd = csv.reader(fd, delimiter="\t", quotechar='"')
     count_default_case_replies = 0
+    count_rows = 0
     for row in rd:
         try:
             if (row[7] == "*" or row[7] == "AIML MATCH FAIL *"):
                 count_default_case_replies += 1
+            count_rows += 1
         except:
             pass
 
 print("There are %s UDC fallbacks in the given logs." %
       count_default_case_replies)
+print("UDC fallbacks make up %s percent of all the bot's responses." %
+      str(round((count_default_case_replies / (count_rows - 1)) * 100)))  # the row count shouldn't include the single header row, so it's subtracted by one
